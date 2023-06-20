54d0fd8669fd6fd2e6c26fe0aad661d2a2162392
FINITO-12 <finito@rocketship.com>
2023-06-20 17:37:47 +0300

    Added REGISTRATION FORM & LOGIN FORM
    IN PROCESS: CHECKING
    
    Tested-by: FINITO-12 <finito@rocketship.com>

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+
+from forms.forms import RegistrationForm, LogInForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '44a2568b40bd2974d01abad1dd4825d9'
 
 # basis = Database("x", "x", "x", "x")
 # conn = basis.connect()
@@ -36,26 +40,47 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
 @app.route("/userprofile")
 def userProfile():
     pass
+
 
 @app.route("/steam")
 def steamTransaction():
     pass
 
+
 @app.route("/steamtl")
 def steamTransactionTL():
     pass
+
 
 @app.route("/minecraft")
 def minecraft():
     pass
 
+
 @app.route("/playstation")
 def psTransaction():
     pass
 
+
 @app.route("/playstationplus")
 def psplusTransaction():
     pass
+
+
+@app.route("/registration", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", 'success')
+        return redirect(url_for('home'))
+    return render_template('registration.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LogInForm()
+    return render_template('login.html', title='Log in', form=form)
